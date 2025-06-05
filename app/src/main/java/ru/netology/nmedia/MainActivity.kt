d80890e4b2511dafd1749aecb30bd66d8fa7dd12
Viktok <viktok162@gmail.com>
2025-06-05 13:15:27 +0500

    Change of heart

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,20 +1,48 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу." +
+                    "Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и" +
+                    " помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное" +
+                    " остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше," +
+                    " целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста" +
+                    " и начать цепочку перемен → http://netolo.gy/fyb\""
+        )
+        with(binding){
+            author.text = post.author
+            content.text = post.content
+            published.text = post.published
+            liked.text = post.likes.toString()
+
+            if (post.likeByMe){
+                heart.setImageResource(R.drawable.heart_full_24dp)
+            }
+
+            heart.setOnClickListener {
+                post.likeByMe = !post.likeByMe
+
+                heart.setImageResource(
+                    if (post.likeByMe){
+                        R.drawable.heart_full_24dp
+                    } else {
+                        R.drawable.heart_empty_24dp
+                    }
+                )
+            }
         }
     }
 }
